4b387cee284607fdd1434d3b2ccff15f280db58b
Micky1129 <136116086+Micky1129@users.noreply.github.com>
2025-05-28 15:40:10 +0800

    docker build frontend

---
+++ docker/frontend/components/layout/BottomNav.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { bottomNavData } from "@/lib/data/bottomNavData";
+import styles from "@/styles/components/layout/BottomNav.module.scss";
+
+export default function BottomNav() {
+  const pathname = usePathname();
+  const isAuthPage = pathname === "/auth";
+  const currentItem = bottomNavData.find((item) =>
+    pathname.startsWith(item.url)
+  );
+
+  const title = currentItem?.title || "";
+
+  if (isAuthPage) return null;
+
+  return (
+    <>
+      <div className={styles.header}>
+        {currentItem && (
+          <>
+            <currentItem.icon size={32} className={styles.focusIcon} />
+            <span className={styles.title}>{currentItem.title}</span>
+          </>
+        )}
+      </div>
+      <div className={styles.navWrap}>
+        {bottomNavData.map((item, index) => {
+          const Icon = item.icon;
+          return (
+            <div className={styles.itemWrap} key={index}>
+              <Link href={item.url}>
+                <Icon
+                  key={index}
+                  size={25}
+                  className={`
+                  ${
+                    item.title === title
+                      ? styles.focusIcon
+                      : styles.notFocusIcon
+                  }`}
+                />
+              </Link>
+              {
+                <div
+                  className={`${styles.mark} ${
+                    title === item.title && styles.focus
+                  }`}
+                />
+              }
+            </div>
+          );
+        })}
+        <Image
+          width={32}
+          height={32}
+          src={"/bottomNavIcon/user.png"}
+          alt={"user"}
+          style={{ borderRadius: "50%", marginBottom: "2%" }}
+        />
+      </div>
+    </>
+  );
+}
