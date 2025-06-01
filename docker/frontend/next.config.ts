7c5261a338bd615e7edb89572a9a27c404d2e933
Micky1129 <coulees.midsts.0t@icloud.com>
2025-06-01 17:39:36 +0800

    update docker frontend

--- docker/frontend/next.config.ts
+++ docker/frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["devapi.micky.codes"],
+  },
 };
 
 export default nextConfig;
