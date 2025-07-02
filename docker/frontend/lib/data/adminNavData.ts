bac6689e184d1f3fc4ec503bf9b3c212db4b562d
senichini <koko930325@gmail.com>
2025-07-03 06:45:15 +0800

    前端 - 新增sidebar

---
+++ docker/frontend/lib/data/adminNavData.ts
@@ -0,0 +1,27 @@
+import { House } from "lucide-react";
+import { Bot } from "lucide-react";
+import { Settings2 } from "lucide-react";
+import { SquareCheckBig } from "lucide-react";
+import { ChartLine } from "lucide-react";
+
+type NavItem = {
+  icon: any;
+  title: string;
+  url: string;
+};
+
+export const navData: NavItem[] = [
+  { icon: House, title: "主頁", url: "/admin" },
+  { icon: SquareCheckBig, title: "核銷報帳", url: "/" },
+  {
+    icon: ChartLine,
+    title: "報表專區",
+    url: "/",
+  },
+  {
+    icon: Bot,
+    title: "FQA智慧回覆",
+    url: "https://line.me/R/ti/p/%40643foras",
+  },
+  { icon: Settings2, title: "其他設定", url: "/setting" },
+];
--- docker/frontend/src/app/admin/page.tsx
+++ docker/frontend/src/app/admin/page.tsx
@@ -3,7 +3,6 @@ import Sidebar from "@/components/layout/Sidebar";
 export default function Admin() {
   return (
     <>
-      <Sidebar />
     </>
   );
 }
