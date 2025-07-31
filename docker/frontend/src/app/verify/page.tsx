0e4db75b5baac1e43d5fccd523d6805e5ac64450
senichini <koko930325@gmail.com>
2025-08-01 00:41:50 +0800

    前端 - 報帳申請畫面

--- docker/frontend/src/app/verify/page.tsx
+++ docker/frontend/src/app/verify/page.tsx
@@ -5,33 +5,51 @@ import { Search } from "lucide-react";
 import { ListChecks } from "lucide-react";
 
 import InputField from "@/components/common/InputField";
-
+import LargeTable from "@/components/LargeTable";
 import styles from "@/styles/app/VerifyPage.module.scss";
 
 export default function Verify() {
   const [isPast, setIsPast] = useState<boolean>(false);
   return (
     <>
-      <div>
-        <p>待核銷報帳</p>
-        <span>|</span>
-        <p>過去核銷紀錄</p>
-      </div>
       <div className={styles.wrap}>
-        <div className={styles.operateBar}>
-          <div className={styles.searchBar}>
-            <InputField type="date" />
-            <InputField type="text" />
-            <InputField type="text" />
-            <button className={styles.searchBtn}>
-              <Search strokeWidth={3} />
+        <div className={styles.title}>
+          <p
+            className={`${!isPast ? styles.focus : styles.normal}`}
+            onClick={() => {
+              setIsPast(false);
+            }}
+          >
+            待核銷報帳
+          </p>
+          <span>|</span>
+          <p
+            className={`${isPast ? styles.focus : styles.normal}`}
+            onClick={() => {
+              setIsPast(true);
+            }}
+          >
+            過去核銷紀錄
+          </p>
+        </div>
+        <div className={styles.searchBar}>
+          <p>報帳申請查詢</p>
+          <div className={styles.operateBar}>
+            <div className={styles.searchItem}>
+              <InputField type="date" />
+              <InputField type="text" />
+              <InputField type="text" />
+              <button className={styles.searchBtn}>
+                <Search strokeWidth={3} />
+              </button>
+            </div>
+            <button className={styles.verifyBtn}>
+              <ListChecks />
+              <p>批量核銷</p>
             </button>
           </div>
-          <button className={styles.verifyBtn}>
-            <ListChecks />
-            批量核銷
-          </button>
         </div>
+        <LargeTable />
       </div>
     </>
   );
