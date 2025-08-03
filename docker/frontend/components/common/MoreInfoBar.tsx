ea7c582bc81180e9d16ea9ad266c69a8572f3d07
senichini <koko930325@gmail.com>
2025-08-03 22:23:52 +0800

    前端 - 修改MoreInfoBar元件

--- docker/frontend/components/common/MoreInfoBar.tsx
+++ docker/frontend/components/common/MoreInfoBar.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
+import styles from "@/styles/components/common/MoreInfoBar.module.scss";
+
 export default function MoreInfoBar({
   title,
   context,
 }: {
   title: string;
-  context: any;
+  context: React.ReactNode;
 }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -14,6 +16,7 @@ export default function MoreInfoBar({
       onClick={() => {
         setIsOpen(isOpen ? false : true);
       }}
+      className={styles.wrap}
     >
       <p>{title}</p>
       {isOpen && <div>{context}</div>}
