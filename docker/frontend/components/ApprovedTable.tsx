86497d6fc39f2b28e9e1720827fb393381c7032f
senichini <koko930325@gmail.com>
2025-09-06 11:35:24 +0800

    前端  -  區分表格元件

---
+++ docker/frontend/components/ApprovedTable.tsx
@@ -0,0 +1,66 @@
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import styles from "@/styles/components/LargeTable.module.scss";
+import { useEffect, useState } from "react";
+import ticketAPI from "@/services/ticketAPI";
+import { pendingTicket } from "@/lib/types/TicketType";
+
+const columns: GridColDef[] = [
+  { field: "upload_date", headerName: "報帳時間", width: 100 },
+  { field: "type", headerName: "報帳種類", width: 100 },
+  { field: "title", headerName: "標題", width: 580 },
+  { field: "total_money", headerName: "金額", width: 50 },
+  {
+    field: "creator_name",
+    headerName: "申請人",
+    width: 70,
+    headerAlign: "center",
+    align: "center",
+  },
+];
+
+export default function ApprovedTable() {
+  const [data, setData] = useState<pendingTicket[]>();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await ticketAPI.getApprove();
+        if (res.data) {
+          setData(res.data);
+        }
+      } catch {}
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div style={{ width: "100%", height: "100%  " }}>
+      <DataGrid
+        className={styles.grid}
+        rows={data}
+        columns={columns}
+        pageSizeOptions={[8]}
+        getRowId={(row) => `${row.upload_date}-${row.img_url}`}
+        disableRowSelectionOnClick
+        initialState={{
+          pagination: {
+            paginationModel: { pageSize: 8, page: 0 },
+          },
+        }}
+        sx={{
+          backgroundColor: "var(--list-bg)",
+          color: "var(--text-color)",
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: "var(--list-bg)",
+            color: "var(--text-color)",
+          },
+          "& .MuiSvgIcon-root": {
+            color: "var(--text-color)",
+          },
+        }}
+      />
+    </div>
+  );
+}
