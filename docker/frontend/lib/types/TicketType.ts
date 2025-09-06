2f4983be77c66506523b7f2bc11eeaed46fd8a9e
senichini <koko930325@gmail.com>
2025-09-06 11:35:40 +0800

    前端 - 新增API

--- docker/frontend/lib/types/TicketType.ts
+++ docker/frontend/lib/types/TicketType.ts
@@ -8,11 +8,41 @@ export type ticketListType = {
   state: string;
 };
 
-export type editBilling = {
+export type editTicket = {
   time: string;
   type: string;
   title: string;
   number: string;
   money: number;
   state: string;
+};
+
+export type auditTicket = {
+  items: {
+    ticket_id: number;
+    status: number;
+  }[];
+};
+
+export type searchTicket = {
+  q?: string;
+  class_info_id?: string;
+  date?: string;
+  limit?: number;
+};
+
+export type latestTicket = {
+  title: string;
+  upload_date: string;
+  type: string | number;
+  total_money: number;
+};
+
+export type pendingTicket = {
+  upload_date: string;
+  type: string | number;
+  title: string;
+  total_money: number;
+  creator_name: string;
+  img_url: string;
 };
--- docker/frontend/services/ticketAPI.ts
+++ docker/frontend/services/ticketAPI.ts
@@ -1,28 +1,60 @@
 import API from "@/lib/api/api";
 import { Response } from "@/lib/types/ResponseType";
-import { ticketListType, editBilling } from "@/lib/types/TicketType";
+import {
+  ticketListType,
+  editTicket,
+  auditTicket,
+  searchTicket,
+  latestTicket,
+  pendingTicket,
+} from "@/lib/types/TicketType";
 
 const BASE_URL = "/ticket";
 
 const ticketAPI = {
   getList: (mode?: number): Promise<Response<ticketListType[]>> =>
     API.get(`${BASE_URL}/list`, { params: { mode: mode } }),
-  getBilling: (id: number): Promise<Response<ticketListType>> =>
+  getTicket: (id: number): Promise<Response<ticketListType>> =>
     API.get(`${BASE_URL}/list/${id}`),
-  addBilling: (data: FormData): Promise<Response<any>> =>
+  addTicket: (data: FormData): Promise<Response<any>> =>
     API.post(`${BASE_URL}/upload`, data, {
       headers: { "Content-Type": "multipart/form-data" },
       toast: true,
     }),
-  editBilling: (id: number, data: editBilling): Promise<Response<any>> =>
+  editTicket: (id: number, data: editTicket): Promise<Response<any>> =>
     API.patch(`${BASE_URL}/change/${id}`, data, {
       headers: { "Content-Type": "application/json" },
       toast: true,
     }),
-  deleteBilling: (id: number): Promise<Response<any>> =>
+  deleteTicket: (id: number): Promise<Response<any>> =>
     API.delete(`${BASE_URL}/delete/${id}`, { toast: true }),
   getUnVerifyCount: (): Promise<Response<any>> =>
-    API.get(`${BASE_URL}/unaudited_invoices`),
+    API.get(`${BASE_URL}/not_write_off`),
+  getLatest: (limit?: number): Promise<Response<latestTicket[]>> =>
+    API.get(`${BASE_URL}/latest_approved`, { params: { limit: limit } }),
+  getPending: (limit?: number): Promise<Response<pendingTicket[]>> =>
+    API.get(`${BASE_URL}/pending_reimbursements`, { params: { limit: limit } }),
+  getApprove: (limit?: number): Promise<Response<any>> =>
+    API.get(`${BASE_URL}/approved_records`, { params: { limit: limit } }),
+  auditTicket: (data: auditTicket): Promise<Response<any>> =>
+    API.patch(`${BASE_URL}/audit`, data, {
+      headers: { "Content-Type": "application/json" },
+      toast: true,
+    }),
+  searchTicket: ({
+    q,
+    class_info_id,
+    date,
+    limit,
+  }: searchTicket): Promise<Response<any>> =>
+    API.get(`${BASE_URL}/search`, {
+      params: {
+        q: q,
+        class_info_id: class_info_id,
+        date: date,
+        limit: limit,
+      },
+    }),
 };
 
 export default ticketAPI;
