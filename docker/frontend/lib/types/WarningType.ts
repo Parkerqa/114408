c8c6c7cd3873faef0340b08ee6581a9d213ca0a3
senichini <koko930325@gmail.com>
2025-09-16 03:57:45 +0800

    前端 - 調整警示燈

--- docker/frontend/lib/types/WarningType.ts
+++ docker/frontend/lib/types/WarningType.ts
@@ -1,12 +1,8 @@
 export type FormValues = {
-  excess_top: number;
-  excess_bot: number;
-  high_top: number;
-  high_bot: number;
-  good_top: number;
-  good_bot: number;
-  low_top: number;
-  low_bot: number;
-  insufficient_top: number;
-  insufficient_bot: number;
+  green_top: number;
+  green_bot: number;
+  yellow_top: number;
+  yellow_bot: number;
+  red_top: number;
+  red_bot: number;
 };
