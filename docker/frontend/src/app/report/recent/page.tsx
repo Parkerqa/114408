ec48bdb77193910144c706ee4cd063400b002a76
senichini <koko930325@gmail.com>
2025-09-18 09:06:28 +0800

    前端 - 建置報表專區頁面

---
+++ docker/frontend/src/app/report/recent/page.tsx
@@ -0,0 +1,10 @@
+import ReportNav from "@/components/ReportNav";
+import styles from "@/styles/app/report/RecentPage.module.scss";
+
+export default function Recent() {
+  return (
+    <div className={styles.wrap}>
+      <ReportNav />
+    </div>
+  );
+}
