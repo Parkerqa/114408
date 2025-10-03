558ea2f872478bffdf363c480812e14ccc2fd806
Micky1129 <coulees.midsts.0t@icloud.com>
2025-10-03 17:16:50 +0800

    前端 - 修改導頁問題

--- docker/frontend/src/app/page.tsx
+++ docker/frontend/src/app/page.tsx
@@ -3,24 +3,28 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
+import { useLoading } from "@/lib/context/LoadingContext";
 import { useConfig } from "@/lib/context/ConfigContext";
 
 export default function Home() {
+  const { setLoading } = useLoading();
   const { user, role } = useConfig();
   const route = useRouter();
 
   useEffect(() => {
     switch (role) {
       case 1:
+        setLoading(false);
         route.replace("/user");
         break;
       case 0:
       case 2:
       case 3:
+        setLoading(false);
         route.replace("/admin");
         break;
       default:
-        route.push("/auth");
+        setLoading(true);
         break;
     }
   }, [user, role]);
